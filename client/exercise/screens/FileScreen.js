be17d20486bc40dda164f435532a130c1b0221ca
Pearce Keesling <keeslinp@gmail.com>
2019-03-25 13:47:33 -0600

    initial goals completed

--- client/exercise/screens/FileScreen.js
+++ client/exercise/screens/FileScreen.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, Dimensions } from 'react-native';
 import { Text } from 'react-native-paper';
+import Pdf from 'react-native-pdf';
 import AppBar from '../components/AppBar';
 
 export default class FileScreen extends React.Component {
 	buildBody = (type, filePath) => {
-		const uri = `http://localhost:3000${filePath}`;
+		const source = { uri: `http://localhost:3000${filePath}` };
 		switch (type) {
 			case 'pdf':
-				return <Text> PDF </Text>;
+				return (
+					<View style={styles.pdfContainer}>
+						<Pdf source={source} style={styles.pdf} />
+					</View>
+				);
 			case 'image':
-				return <Image source={{ uri }} style={styles.imageStyle} />;
-      default:
+				return <Image source={source} style={styles.imageStyle} />;
+			default:
 				return <Text> Unknown file type </Text>;
 		}
 	};
@@ -19,8 +24,14 @@ export default class FileScreen extends React.Component {
 		const id = this.props.navigation.getParam('id', null);
 		const documentObj = this.props.screenProps.documentList.find(doc => doc.id === id);
 		const { name, size, type, self } = documentObj;
+		// const source = { uri: `http://localhost:3000${self}` };
+		// return (
+		// <View style={styles.container}>
+		// <Pdf source={source} style={styles.pdf} />
+		// </View>
+		// );
 		return (
-			<View>
+			<View style={styles.container}>
 				<AppBar title={name} goBack={() => this.props.navigation.goBack()}>
 					<Text style={styles.fileSize}>{size}</Text>
 				</AppBar>
@@ -36,7 +47,22 @@ const styles = StyleSheet.create({
 		marginRight: 10,
 	},
 	imageStyle: {
-    width: 100,
-		height: 100,
+		marginTop: 10,
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height / 1.5,
 	},
+	container: {
+		flex: 1,
+	},
+	pdfContainer: {
+		flex: 1,
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		marginTop: 25,
+		zIndex: -1,
+	},
+	pdf: {
+		flex: 1,
+		width: Dimensions.get('window').width,
+	}
 });
